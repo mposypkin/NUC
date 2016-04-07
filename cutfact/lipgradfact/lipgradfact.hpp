c96f2943ae50a2c54a71d46ea4f8dec03bd38f2b
Mikhail Posypkin <mposypkin@gmail.com>
2016-04-07 19:19:55 +0300

    Cut factories to take gradient into account

---
+++ cutfact/lipgradfact/lipgradfact.hpp
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   lipgradfact.hpp
+ * Author: Mikhail Posypkin <mposypkin at gmail.com>
+ *
+ * Created on 7 апреля 2016 г., 16:52
+ */
+
+#ifndef LIPGRADFACT_HPP
+#define LIPGRADFACT_HPP
+
+
+
+#endif /* LIPGRADFACT_HPP */
+
---
+++ cutfact/lipgradfact/lpzgradsupp.hpp
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   lpzgradsupp.hpp
+ * Author: Mikhail Posypkin <mposypkin at gmail.com>
+ *
+ * Created on 7 апреля 2016 г., 16:42
+ */
+
+#ifndef LPZGRADSUPP_HPP
+#define LPZGRADSUPP_HPP
+
+#include <box/box.hpp>
+
+/**
+ * Supplies Lipzshitzian constants for gradient
+ */
+template <class FT> class LpzGradSupp {
+
+    public:   
+        /**
+         * Retrieve Lipschitzian constants for gradient components
+         * @param box the source box
+         * @param lpzv the vector of Lipschitzian constants
+         */
+        virtual void getLpzConst(const snowgoose::Box<FT>& box, FT* lpzv) = 0;
+
+};
+
+#endif /* LPZGRADSUPP_HPP */
+
