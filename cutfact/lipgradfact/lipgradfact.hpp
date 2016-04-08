3a2179d5ec5df3541f2dbb42862ae7717a437d70
Mikhail Posypkin <mposypkin@gmail.com>
2016-04-09 00:55:13 +0400

    Updated Lipschitzian gradient

--- cutfact/lipgradfact/lipgradfact.hpp
+++ cutfact/lipgradfact/lipgradfact.hpp
@@ -14,7 +14,69 @@
 #ifndef LIPGRADFACT_HPP
 #define LIPGRADFACT_HPP
 
+#include <functor.hpp>
+#include <box/box.hpp>
+#include <box/boxutils.hpp>
+#include <cutfact/common/cutfactory.hpp>
+#include <common/smartptr.hpp>
+#include <cuts/totalcut.hpp>
+#include "lpzgradsupp.hpp"
 
+namespace NUC {
+
+    /**
+     * Cut factory based on Lipschitzian gradients
+     */
+    template <class FT> class LipGradCutFactory : public CutFactory <FT> {
+    public:
+
+        /**
+         * Constructor 
+         * @param supp the reference to the supplier
+         * @param f reference to the functor
+         * @param box problem bounding box
+         * @param delta tolerance to distinguish grad from 0
+         */
+        LipGradCutFactory(COMPI::Functor<FT>& f, const snowgoose::Box<FT>& box, LpzGradSupp<FT>& supp, FT delta = 1e-8) : mF(f), mBox(box), mGradSupp(supp), mDelta(delta) {
+            const int n = box.mDim;
+            mX.alloc(n);
+            mG.alloc(n);
+            mLG.alloc(n);
+        }
+
+        void getCuts(const snowgoose::Box<FT>& box, std::vector<std::shared_ptr <Cut <FT> > >& v) const {
+            const int n = mBox.mDim;
+
+            snowgoose::BoxUtils::getCenter(box, (FT*) mX);
+            FT r = snowgoose::BoxUtils::radius(box);
+            mF.grad(mX, mG);
+            mGradSupp.getLpzConst(box, mLG);
+            bool totcat = false;
+            for (int i = 0; i < n; i++) {
+                FT lb = mG[i] - r * mLG[i];
+                FT ub = mG[i] + r * mLG[i];
+                if (lb > mDelta) {
+                    totcat = true;
+                } else if (ub < -mDelta) {
+                    totcat = true;
+                }
+                if (totcat) {
+                    TotalCut<FT> *tc = new TotalCut <FT> (box);
+                    std::shared_ptr< Cut<FT> > pc(tc);
+                    v.push_back(pc);
+                }
+            }
+        }
+
+        COMPI::Functor<FT>& mF;
+        const snowgoose::Box<FT>& mBox;
+        LpzGradSupp<FT>& mGradSupp;
+        snowgoose::SmartArrayPtr<FT> mX;
+        snowgoose::SmartArrayPtr<FT> mG;
+        snowgoose::SmartArrayPtr<FT> mLG;
+        FT mDelta;
+    };
+}
 
 #endif /* LIPGRADFACT_HPP */
 
--- cutfact/lipgradfact/testlipgradfact.cpp
+++ cutfact/lipgradfact/testlipgradfact.cpp
@@ -5,20 +5,56 @@
  */
 
 #include <oneobj/contboxconstr/alufpent.hpp>
+#include <common/interval.hpp>
 #include "lpzgradsupp.hpp"
+#include "lipgradfact.hpp"
 
 class APSupp : public NUC::LpzGradSupp <double> {
-    public:
+public:
+
     void getLpzConst(const snowgoose::Box<double>& box, double* lpzv) {
-        const int n = box.mDim;
-        for(int i = 0; i < n; i ++) {
-            // TMP
-            lpzv[i] = 1.;
-        }
+        double a = box.mA[0];
+        double b = box.mB[0];
+        snowgoose::Interval<double>::degree(2, a, b, &a, &b);
+        a *= 3;
+        b *= 3;
+        a -= 1;
+        b -= 1;
+        lpzv[0] = SGMAX(SGABS(a), SGABS(b));
+        lpzv[1] = 0;
     }
+
 };
 
+void initbox(snowgoose::Box<double>& box) {
+    const int n = box.mDim;
+    for (int i = 0; i < n; i++) {
+        box.mA[i] = -2;
+        box.mB[i] = 2;
+    }
+}
+
+void initcbox(snowgoose::Box<double>& box) {
+    const int n = box.mDim;
+    for (int i = 0; i < n; i++) {
+        box.mA[i] = 1;
+        box.mB[i] = 2;
+    }
+}
+
 int main() {
+    const int n = 2;
     OPTITEST::AluffiPentiniObjective obj;
+    APSupp gsupp;
+    snowgoose::Box<double> box(n);
+    snowgoose::Box<double> cbox(n);
+    initbox(box);
+    initcbox(cbox);
+    NUC::LipGradCutFactory<double> lfact(obj, box, gsupp);
+    std::vector<std::shared_ptr <NUC::Cut <double> > > cv;
+    lfact.getCuts(cbox, cv);
+    for(auto o : cv) {
+        std::cout << "Cut of type: " << o.get()->about() << "\n";
+    }
     return 0;
 }
